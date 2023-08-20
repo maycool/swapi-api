d5e528f45b32eb5af0285ada35a27f41f0900084
Michael Magdy <michael.magdy@coformatique.com>
2023-08-21 00:34:46 +0300

    chore: refactor

--- src/error-handler.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
-  console.error('An error occurred:', err.message);
-  res.status(500).json({ error: 'Internal server error' });
-}
